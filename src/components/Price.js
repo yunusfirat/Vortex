e6e0b059ff3463c36a4f1a89e19500223d02a856
Yunus Firat <yunusfirat@Yunuss-MacBook-Air.local>
2022-02-02 02:49:22 +0300

    project updated and completed

--- src/components/Price.js
+++ src/components/Price.js
@@ -4,50 +4,52 @@ import { useState, useEffect } from 'react'
 
 export default function Price() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const socket = new W3CWebSocket(
-    'wss://stream.tradingeconomics.com/?client=guest:guest',
-  )
-  const [data, setData] = useState({})
-  let subRequest = {
-    topic: 'subscribe',
-    to: 'EURUSD:CUR',
-  }
+  const [data, setData] = useState('')
 
-  socket.onopen = function () {
-    console.log('Opened connection 🎉')
-
-    // send data to the server
-    var json = JSON.stringify(subRequest)
-    socket.send(json)
-  }
   useEffect(() => {
+    console.log('Hello World!')
+    const socket = new W3CWebSocket(
+      'wss://stream.tradingeconomics.com/?client=guest:guest',
+    )
+    let subRequest = {
+      topic: 'subscribe',
+      to: 'EURUSD:CUR',
+    }
+    socket.onopen = function () {
+      // console.log('Opened connection 🎉')
+      // send data to the server
+      var json = JSON.stringify(subRequest)
+      socket.send(json)
+    }
     // When data is received
     socket.onmessage = function (event) {
       // @ts-ignore
       let finaldata = JSON.parse(event.data)
-      if (finaldata) {
-        setData(finaldata)
-      } else {
-        setData({
-          message: 'loading',
-        })
+      try {
+        // eslint-disable-next-line no-cond-assign
+        if (finaldata.event = "data") {
+          setData(finaldata)
+        }
+      } catch (error) {
+        console.log(error)
       }
     }
-  }, [socket])
-  // A connection could not be made
-  socket.onerror = function (event) {
-    console.log(event)
-  }
-  socket.onclose = function (code, reason) {
-    console.log(code, reason)
-  }
+    // A connection could not be made
+    socket.onerror = function (event) {
+      console.log(event)
+    }
+    socket.onclose = function (code, reason) {
+      console.log(code, reason)
+    }
+  }, [])
+
   //   console.log("finaldata", data.price)
   let timestamp = data
     ? new Date(data.dt).toLocaleTimeString('en-UK')
     : 'Loading'
-//   console.log(timestamp)
+  //   console.log(timestamp)
 
-//   console.log('time', timestamp)
+  //   console.log('time', timestamp)
   return (
     <div>
       <h2>EUR/USD:{data ? data.price : 'Loading'}</h2>
