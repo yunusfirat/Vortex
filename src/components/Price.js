113c7ab70d6e69ada268161da81d82a563c4acac
Yunus Firat <yunusfirat@Yunuss-MacBook-Air.local>
2022-02-01 17:24:44 +0300

    missing task has been completed

--- src/components/Price.js
+++ src/components/Price.js
@@ -1,47 +1,59 @@
-// import { w3cwebsocket as W3CWebSocket } from 'websocket'
-// import { useState } from 'react'
+// @ts-nocheck
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
+import { useState, useEffect } from 'react'
 
-// export default function Price() {
-//   const socket = new W3CWebSocket(
-//     'ws://stream.tradingeconomics.com/?client=guest:guest',
-//   )
-//   const [data, setData] = useState('')
-//   let subRequest = {
-//     topic: 'subscribe',
-//     to: 'EURUSD:CUR',
-//   }
+export default function Price() {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const socket = new W3CWebSocket(
+    'ws://stream.tradingeconomics.com/?client=guest:guest',
+  )
+  const [data, setData] = useState({})
+  let subRequest = {
+    topic: 'subscribe',
+    to: 'EURUSD:CUR',
+  }
 
-//   socket.onopen = function () {
-//     console.log('Opened connection 🎉')
+  socket.onopen = function () {
+    console.log('Opened connection 🎉')
 
-//     // send data to the server
-//     var json = JSON.stringify(subRequest)
-//     socket.send(json)
-//   }
+    // send data to the server
+    var json = JSON.stringify(subRequest)
+    socket.send(json)
+  }
+  useEffect(() => {
+    // When data is received
+    socket.onmessage = function (event) {
+      // @ts-ignore
+      let finaldata = JSON.parse(event.data)
+      if (finaldata) {
+        setData(finaldata)
+      } else {
+        setData({
+          message: 'loading',
+        })
+      }
+    }
+  }, [socket])
+  // A connection could not be made
+  socket.onerror = function (event) {
+    console.log(event)
+  }
+  socket.onclose = function (code, reason) {
+    console.log(code, reason)
+  }
+  //   console.log("finaldata", data.price)
+  let timestamp = data
+    ? new Date(data.dt).toLocaleTimeString('en-UK')
+    : 'Loading'
+  console.log(timestamp)
 
-//   // When data is received
-//   socket.onmessage = function (event) {
-//     console.log('data', event.data)
-//     setData(event.data)
-//   }
-//   console.log(data)
-//   // A connection could not be made
-//   socket.onerror = function (event) {
-//     console.log(event)
-//   }
-//   socket.onclose = function (code, reason) {
-//     console.log(code, reason)
-//   }
-
-//   return (
-//     <div>
-//       <h2>{data}</h2>
-//     </div>
-//   )
-// }
-
-//  Somewhere on the page you should show the latest price and the latest timestamp (in local time for the
-//     user) for the EUR/USD exchange rate from a websocket feed.
-//     - The feed can be accessed by sending “{"topic": "subscribe", "to": "EURUSD:CUR"}” to
-//     ws://stream.tradingeconomics.com/?client=guest:guest
-//     - ‘dt’ is the timestamp as UTC and ‘price’ is the latest price.
+  console.log('time', timestamp)
+  return (
+    <div>
+      <h2>EUR/USD:{data ? data.price : 'Loading'}</h2>
+      <div>
+        <h2>time:{timestamp === 'Invalid Date' ? 'Loading' : timestamp}</h2>
+      </div>
+    </div>
+  )
+}
